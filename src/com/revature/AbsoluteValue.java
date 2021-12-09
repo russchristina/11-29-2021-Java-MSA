ca088e9f65648bc0f13e2509a89eb331151c0d33
S4194 <wilson_shamarr@columbusstate.edu>
2021-12-08 19:11:59 -0500

    Week 2 updated

--- src/com/revature/AbsoluteValue.java
+++ src/com/revature/AbsoluteValue.java
@@ -2,18 +2,23 @@ package com.revature;
 
 public class AbsoluteValue {
 	
-	public int calculateAbsoluteValueofDifference(int num1, int num2) {
-		return (num1 -num2 >=0) ?
-	
-		 int difference = num1 - num2;
-		 
-		 /*
-		  * Never want negative number so you should multiply result be -1.
-		  */
-		 
-		 if(difference >=0) return difference;
-		 
-		 else return difference * -1;
+	public static void main(String []args) {
+		AbsoluteValue absoluteValue = new AbsoluteValue();
+		
+		System.out.println(absoluteValue.calculateDifAbsoluteValue(-3,7));
 	}
-
+public int calculateDifAbsoluteValue(int num1, int num2) {
+	Math.abs(num1);
+	
+	
+	
+	int difference= num1 -num2;
+	
+	if (difference>=0) 
+		return difference;
+		
+	else
+		return difference *-1;
+			
+	}
 }
--- src/com/revature/Sum.java
+++ src/com/revature/Sum.java
@@ -1,5 +1,22 @@
 package com.revature;
 
 public class Sum {
+	
+	public static void main(String []args) {
+		Sum sumValue = new Sum();
+		System.out.println(sumValue.add(1,2,3,4,5));
+	
+	
 
+}
+
+public int add(int...numbers) {
+		
+		int sum=0;
+		
+		for(int i:numbers) {
+		sum +=i;
+	}
+		return sum;
+	}
 }
