9f2454bf995856770e0323b58aa44b466ca686cb
russ.christinas <russ.christinas@gmail.com>
2021-12-17 21:16:14 -0600

    refactored code for better resource cleanup with try with resources

--- Week 3/Examples/Java Basics/src/com/revature/scannerandfileio/FileInputAndOutput.java
+++ Week 3/Examples/Java Basics/src/com/revature/scannerandfileio/FileInputAndOutput.java
@@ -42,32 +42,24 @@ public class FileInputAndOutput {
 		//If this file does not exist, it will be created in my workspace.
 		File myFile = new File("people.txt");
 		
-		try {
+		/*
+		 * If you pass in a boolean value of true, this writer will append to the file
+		 * rather than overwriting its contents.
+		 * 
+		 * I recommend wrapping the FileWriter inside of a BufferedWriter; this will
+		 * allow you to write a line rather than just a single character.
+		 */
+		try(FileWriter writer = new FileWriter(myFile, true); BufferedWriter buffWriter = new BufferedWriter(writer)) {
 			//Here for your reference. This is more useful for writing images.
 //			FileOutputStream fos = new FileOutputStream(myFile);
-			/*
-			 * If you pass in a boolean value of true, this writer will append to the file
-			 * rather than overwriting its contents.
-			 */
-			FileWriter writer = new FileWriter(myFile, true);
-			/*
-			 * I recommend wrapping the FileWriter inside of a BufferedWriter; this will
-			 * allow you to write a line rather than just a single character.
-			 */
-			BufferedWriter buffWriter = new BufferedWriter(writer);
 			
 			/*
 			 * Now let's just write the user's full name to the file:
 			 */
 			buffWriter.write(fullName + "\n");
-			/*
-			 * Every resource you open, you must close.
-			 */
-			buffWriter.close();
-			writer.close();
 			
 			/*
-			 * As far as FileIO, you have to handle FileNotFoundExceptions as they
+			 * As far as FileIO goes, you have to handle FileNotFoundExceptions as they
 			 * are checked exceptions.
 			 */
 		} catch (IOException e) {
@@ -82,30 +74,19 @@ public class FileInputAndOutput {
 		 * - FileReader - reads characters from a file
 		 * - BufferedReader - reads entire lines from a file; it is thread-safe 
 		 */
-		FileReader reader;
-		BufferedReader buffReader;
 		
-		try {
+		try(FileReader reader = new FileReader(myFile); BufferedReader buffReader = new BufferedReader(reader)) {
 			//Again, this is more recommended for reading in bytes for images.
 //			FileInputStream fis = new FileInputStream(myFile);
-			reader = new FileReader(myFile);
-			buffReader = new BufferedReader(reader);
 			
 			while(buffReader.ready()) {
 				System.out.println(buffReader.readLine());
 			}
 			
-			/*
-			 * Always close your resources.
-			 */
-			buffReader.close();
-			reader.close();
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		
 	}
 
