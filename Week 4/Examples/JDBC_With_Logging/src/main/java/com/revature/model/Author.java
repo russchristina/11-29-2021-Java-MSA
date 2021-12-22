5cb0b870287821a85f13d8b5d55d64b491b59544
russ.christinas <russ.christinas@gmail.com>
2021-12-22 11:37:50 -0600

    jdbc

--- Week 4/Examples/JDBC_With_Logging/src/main/java/com/revature/model/Author.java
+++ Week 4/Examples/JDBC_With_Logging/src/main/java/com/revature/model/Author.java
@@ -5,4 +5,80 @@ public class Author {
 	private int id;
 	private String name;
 	private String specialty;
+
+	public Author() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Author(int id, String name, String specialty) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.specialty = specialty;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((specialty == null) ? 0 : specialty.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (specialty == null) {
+			if (other.specialty != null)
+				return false;
+		} else if (!specialty.equals(other.specialty))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", name=" + name + ", specialty=" + specialty + "]";
+	}
+
 }
