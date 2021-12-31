c724223d9280cb5d9652b7e1e32813df7655bfa2
DementedTiger <72563448+DementedTiger@users.noreply.github.com>
2021-12-31 00:40:44 -0600

    first stage

---
+++ Project Submissions/HeroPay/createTableScript.sql
@@ -0,0 +1,69 @@
+
+
+select * from employee_account;
+drop table employee_account;
+
+create table employee_account(
+id serial primary key,
+first_name varchar,
+last_name varchar,
+role integer references employee_role(id)
+);
+
+
+select * from employee_role;
+drop table employee_role;
+
+create table employee_role(
+id serial primary key,
+role_name varchar unique not null
+);
+
+
+select * from login_info;
+drop table login_info;
+
+create table login_info(
+id serial primary key,
+username varchar unique not null,
+password varchar not null,
+employee_id integer references employee_account(id)
+);
+
+
+select * from pending_request;
+drop table pending_request;
+
+create table pending_request(
+id serial primary key,
+employee_id integer references employee_account(id),
+type integer references request_type(id),
+request_message varchar,
+amount numeric not null,
+date_submission date not null
+);
+
+select * from request_type;
+drop table request_type;
+
+create table request_type(
+id serial primary key,
+type varchar unique not null
+);
+
+
+select * from completed_request;
+drop table completed_request;
+
+create table completed_request(
+id serial primary key,
+manager_id integer references employee_account(id),
+status boolean,
+response varchar,
+date_resolved date not null
+);
+
+
+
+
+
---
+++ Project Submissions/HeroPay/populateTable.sql
@@ -0,0 +1,26 @@
+insert into employee_role values(default, 'Knight'), (default, 'Cleric'), (default, 'Mage'), (default, 'Manager');
+
+
+
+insert into employee_account values(default, 'Emerick' , 'Ademar', 1), (default, 'William', 'Johnson', 1), (default, 'Gwyndolyn', 'Braveheart', 1);
+
+insert into employee_account values(default, 'Gustave' , 'Khihrer', 2), (default, 'Terese', 'Stoutcrag', 2), (default, 'Haines', 'Nakein', 2);
+
+insert into employee_account values(default, 'Iphazor' , 'Thitarum', 3), (default, 'Amari', 'Rhidrisse', 3), (default, 'Enumari', 'Obaris', 3);
+
+insert into employee_account values(default, 'John' , 'Smith', 4), (default, 'Linda', 'Gobert', 4);
+
+
+
+insert into login_info values
+(default, 'knight1', 'pass1', 1), 
+(default, 'knight2', 'pass2', 2),
+(default, 'knight3', 'pass3', 3),
+(default, 'cleric1', 'pass4', 4),
+(default, 'cleric2', 'pass5', 5),
+(default, 'cleric3', 'pass6', 6),
+(default, 'mage1', 'pass7', 7),
+(default, 'mage2', 'pass8', 8),
+(default, 'mage3', 'pass9', 9),
+(default, 'manager1', 'pass10', 10),
+(default, 'manager2', 'pass11', 11);
